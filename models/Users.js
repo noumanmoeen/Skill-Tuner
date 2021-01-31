7560d62b9146bcb6a51a2ded5f22b045ec65e0ac
Mhassanmustafa <sp17-bcs-065@cuilahore.edu.pk>
2021-01-31 12:37:50 +0500

    auth added

---
+++ models/Users.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const { APIError } = require("../helpers/error");
+
+const userschema = mongoose.Schema(
+  {
+    firstname: String,
+    lastname: String,
+    username: {
+      type: String,
+      index: true,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: function (value) {
+        return validator.isEmail(value);
+      },
+    },
+    pswd: {
+      type: String,
+      required: true,
+      set: function (value) {
+        return bcrypt.hashSync(value, 10);
+      },
+      phone: {
+        type: String,
+        unique: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userschema.method("createUser", async function (user) {
+  let User = this.model("User");
+  if ((await User.findOne({ email: user.email })) !== null) {
+    throw new APIError(400, "User with this email already exists");
+  }
+  if ((await User.findOne({ username: user.username })) !== null) {
+    throw new APIError(400, "User with this username already exists");
+  }
+
+  if ((await User.findOne({ reg_no: user.reg_no })) !== null) {
+    throw new APIError(400, "User with this registeration no already exists");
+  }
+
+  return await User.create(user);
+});
+
+userschema.method("checkIfUserWithEmailExists", async function (email) {
+  let User = this.model("User");
+  let user = await User.findOne({ email });
+
+  if (user == null) {
+    throw new APIError(404, "No user with this email exists");
+  }
+  return user;
+});
+
+userschema.method("checkIfUserWithUsernameExists", async function (username) {
+  let User = this.model("User");
+  return await User.findOne({ username }).select("username");
+});
+
+userschema.method("getUser", async function (_id) {
+  let User = this.model("User");
+  let user = await User.findOne({ _id }).select("-pswd");
+  if (!user) {
+    throw new APIError(404, "No user found");
+  }
+  return user;
+});
+
+userschema.method("checkPass", function (user, pswd) {
+  return bcrypt.compareSync(pswd, user.pswd);
+});
+
+userschema.method("deleteUser", async function (_id) {
+  return await this.model("User").deleteOne({ _id });
+});
+
+module.exports = mongoose.model("User", userschema);
---
+++ routes/routers/users.js
@@ -0,0 +1,126 @@
+const exjwt = require("express-jwt");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const process = require("process");
+const User = require("./../../models/Users");
+
+const keys = {
+  jwtsecret: process.env.jwtsecret,
+};
+
+const ejwtauth = exjwt({ secret: keys.jwtsecret, algorithms: ["HS256"] });
+
+const router = express.Router();
+const validateObjectID = require("mongoose").Types.ObjectId.isValid;
+const { processValidationErrors, APIError } = require("../../helpers/error");
+const { param, body } = require("express-validator");
+
+/*
+  --------
+  Create
+  --------
+*/
+
+router.post(
+  "/users/register",
+  body("firstname").escape(),
+  body("lastname").escape(),
+  body(
+    "username",
+    "Username is required and only alpha numeric characters are allowed"
+  ).isAlphanumeric(),
+  body("email", "Email is required").isEmail().normalizeEmail(),
+  body(
+    "pswd",
+    "Password is required, must contain atleast 6 characters, and must be a string."
+  )
+    .isLength({ min: 6 })
+    .isString(),
+
+  (req, res, next) => {
+    let user = new User();
+    user
+      .createUser(req.body)
+      .then((data) => {
+        res.send({ message: "User registered" });
+      })
+      .catch(next);
+  }
+);
+
+/*
+  --------
+  Read
+  --------
+*/
+
+router.post(
+  "/users/login",
+  body("email", "Email is required and must be a valid mail")
+    .isEmail()
+    .normalizeEmail(),
+  body(
+    "pswd",
+    "Password is required, must contain atleast 6 characters, and must be a string."
+  )
+    .isLength({ min: 6 })
+    .isString(),
+  processValidationErrors,
+  (req, res, next) => {
+    let user = new User();
+    user
+      .checkIfUserWithEmailExists(req.body.email)
+      .then((_user) => {
+        // verify password
+        if (user.checkPass(_user, req.body.pswd)) {
+          let token = jwt.sign(
+            {
+              _id: _user._id,
+              username: _user.username,
+              role: _user.role,
+            },
+            keys.jwtsecret
+          );
+          // TODO: never expiring tokens
+          res.send({
+            _id: _user._id,
+            token,
+            message: "Keep it safe :)",
+          });
+        } else {
+          next(new APIError(400, "Invalid Password"));
+        }
+      })
+      .catch(next);
+  }
+);
+
+router.get(
+  "/users/:id",
+  param("id", "Invalid Object ID")
+    .escape()
+    .custom((value) => validateObjectID(value)),
+  processValidationErrors,
+  (req, res, next) => {
+    let user = new User();
+    user
+      .getUser(req.params.id)
+      .then((data) => res.send(data))
+      .catch(next);
+  }
+);
+
+router.get(
+  "/users/checkusername/:username",
+  param("username").escape().isString(),
+  processValidationErrors,
+  (req, res, next) => {
+    let user = new User({ username: req.params.username });
+    user
+      .checkIfUserWithUsernameExists(this.user)
+      .then((data) => res.send(data ? 200 : 400))
+      .catch(next);
+  }
+);
+
+module.exports = router;
