fb4a39e62e61bc282bd8646e158abce6b75f4809
Mhassanmustafa <sp17-bcs-065@cuilahore.edu.pk>
2021-03-20 19:51:42 +0500

    added graph structure in dashboard yet to be connected with mongodb

--- client/src/Compnents/Dashboard.jsx
+++ client/src/Compnents/Dashboard.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import auth_axios from "../utils/auth_axios";
-
+import { Bar } from "react-chartjs-2";
+// todo: adding data in charts comming from mongodb
 class Dashboard extends Component {
   constructor(props) {
     super(props);
@@ -10,6 +11,32 @@ class Dashboard extends Component {
       noOfUsers: 0,
       noOfBlock: 0,
       noOfCourses: 0,
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "to be decided",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
     };
   }
 
@@ -119,7 +146,7 @@ class Dashboard extends Component {
                   </div>
                 </section>
                 <section id="chart" className="p-4">
-                  <canvas id="myChart" width={200} height={200} />
+                  <Bar data={this.state.data} />
                 </section>
               </div>
             </main>
