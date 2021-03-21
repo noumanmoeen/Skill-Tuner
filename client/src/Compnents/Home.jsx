853a00efa1894196783246f97d6fc7caaea27568
Mhassanmustafa <sp17-bcs-065@cuilahore.edu.pk>
2021-03-21 23:45:10 +0500

    search bar ui added

--- client/src/Compnents/Home.jsx
+++ client/src/Compnents/Home.jsx
@@ -30,6 +30,40 @@ class Home extends Component {
             </div>
           </div>
         </section>
+        <div className="container">
+          <div
+            className="rounded p-3 lg:p-4 relative mt-n5 z-20"
+            style={{
+              background: "#fff",
+              boxShadow: "0 0 1rem rgb(0 0 0 / 15%)",
+            }}
+          >
+            <div className="w-2/3 inline-block">
+              <input
+                className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-grey"
+                id="grid-password"
+                type="search"
+                placeholder="Search over 1000s of free courses and videos..."
+              />
+            </div>
+
+            <div className="inline-block w-1/6 ml-2">
+              <select
+                className="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-grey outline-none"
+                id="grid-state"
+              >
+                <option>All</option>
+                <option>Courses</option>
+                <option>Lecture Title</option>
+              </select>
+            </div>
+            <div className="inline-block w-1/7 ml-1">
+              <button className="focus:outline-none text-white text-sm py-3 px-5 rounded-md bg-gradient-to-r from-blue-400 to-blue-600 transform hover:scale-110">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
